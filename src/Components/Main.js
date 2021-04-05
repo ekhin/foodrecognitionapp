8b0f4d7225a1246dffbe248513b609c0a1b9ff7e
Samantha Khin <waikhin10@gmail.com>
2021-04-04 19:42:02 -0700

    edited nutrient part

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,7 +5,7 @@ import FoodInfo from './FoodInfo';
 import Uploads from './Uploads';
 import Cookies from 'universal-cookie';
 
-import { Navbar } from 'rsuite';
+
 
 
 
@@ -15,11 +15,6 @@ export default class Main extends React.Component {
   render() {
     return (
       <div>
-        <Navbar bg="dark">
-          <Navbar.Header>
-            
-          </Navbar.Header>
-        </Navbar>
         <h1> Estimate My Daily Intake! </h1>
         <Uploads/>
       </div>
